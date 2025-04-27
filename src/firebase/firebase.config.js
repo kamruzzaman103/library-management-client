b6ddb7c3d9c8021e14e54f20dfdc2acb49f7fc20
kamruzzaman103 <kamruzzaman714@gmail.com>
2025-04-27 22:42:38 +0600

    added env

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,14 +1,11 @@
-// Replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyB9V4I0K18KdKHZ0pAR4YcCEGFmDnPLs-0",
-  authDomain: "library-management-syste-55454.firebaseapp.com",
-  projectId: "library-management-syste-55454",
-  storageBucket: "library-management-syste-55454.firebasestorage.app",
-  messagingSenderId: "361378754418",
-  appId: "1:361378754418:web:068ef78d7979137811b7eb",
-  measurementId: "G-47P1R185YH",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 export default firebaseConfig;
-
-
